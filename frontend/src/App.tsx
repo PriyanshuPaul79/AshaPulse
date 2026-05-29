c2d9b8eba5e003ac2f181fc55632722f594aebb6
PriyanshuPaul79 <priyanshupaul32@gmail.com>
2026-05-29 21:03:09 +0530

    add: Updated logos and added dark mode.

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -4,6 +4,7 @@
  */
 
 import { HashRouter, Routes, Route } from 'react-router-dom';
+import { ThemeProvider } from './lib/theme';
 import AppShell from './components/layout/AppShell';
 import HomePage from './pages/HomePage';
 import ResultPage from './pages/ResultPage';
@@ -12,15 +13,18 @@ import HistoryPage from './pages/HistoryPage';
 
 export default function App() {
   return (
-    <HashRouter>
-      <Routes>
-        <Route path="/" element={<AppShell />}>
-          <Route index element={<HomePage />} />
-          <Route path="result" element={<ResultPage />} />
-          <Route path="phc" element={<PhcPage />} />
-          <Route path="history" element={<HistoryPage />} />
-        </Route>
-      </Routes>
-    </HashRouter>
+    <ThemeProvider>
+      <HashRouter>
+        <Routes>
+          <Route path="/" element={<AppShell />}>
+            <Route index element={<HomePage />} />
+            <Route path="result" element={<ResultPage />} />
+            <Route path="phc" element={<PhcPage />} />
+            <Route path="history" element={<HistoryPage />} />
+          </Route>
+        </Routes>
+      </HashRouter>
+    </ThemeProvider>
   );
 }
+
