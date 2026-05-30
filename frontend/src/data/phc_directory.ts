edb913afe5ac3a58e00b18a34b60f02e5910bf8e
PriyanshuPaul79 <priyanshupaul32@gmail.com>
2026-05-30 12:10:08 +0530

    add: Added all the important files.

---
+++ frontend/src/data/phc_directory.ts
@@ -0,0 +1,22 @@
+export const PHC_DIRECTORY = {
+  "Paschim Bardhaman": [
+    {
+      "id": "WB-PWB-001",
+      "name": "Andal Primary Health Centre",
+      "block": "Andal",
+      "address": "Andal, Paschim Bardhaman, West Bengal",
+      "pin": "713321",
+      "contact": null,
+      "open_24hr": false,
+      "timing": "9AM-4PM Mon-Sat",
+      "services": ["OPD", "Maternal & Child Health", "Malaria Testing"],
+      "ambulance": false,
+      "lat": 23.5937,
+      "lng": 87.1824
+    }
+  ],
+  "Purba Bardhaman": [],
+  "Hooghly": [],
+  "Howrah": [],
+  "Kolkata": []
+};
